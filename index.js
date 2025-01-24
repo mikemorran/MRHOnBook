d12caed15853879704520c5a7f1b91d546a3f11c
Michael Morran <67967136+mikemorran@users.noreply.github.com>
2025-01-24 17:32:14 -0500

    processing json key

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const admin = require('firebase-admin');
-const serviceAccount = process.env.KEY_JSON || require('./key.json');
-console.log(serviceAccount)
+const serviceAccount = JSON.parse(process.env.KEY_JSON) || require('./key.json');
 
 const app = express();
 const server = http.createServer(app);
